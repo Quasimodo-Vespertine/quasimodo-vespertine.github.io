11c7970cd372e10ff0232ea6397f49844f9fa7e3
sambodjango <gringolalia@gmail.com>
2021-01-10 12:11:49 -0300

    deploy

---
+++ precache-manifest.c903f8ca72d8652b1554173e9daf43c9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c7f989d20f4b4dc6d0",
+    "url": "/css/app.33718189.css"
+  },
+  {
+    "revision": "6e5cf45d66448e551f03",
+    "url": "/css/chunk-vendors.6b88b602.css"
+  },
+  {
+    "revision": "39083cdef834fe0d7ee39e0272d1aa76",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.39083cde.woff"
+  },
+  {
+    "revision": "d960b795b343e35150c1a0aa3c38c16b",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.d960b795.woff2"
+  },
+  {
+    "revision": "8c4120b4e88276badbc808e518572fad",
+    "url": "/img/logo.8c4120b4.png"
+  },
+  {
+    "revision": "5333754f3dbddd51b4d9e9fc71c9e482",
+    "url": "/img/nyc-dynamo.5333754f.jpg"
+  },
+  {
+    "revision": "3681a0eaf7e7e6ebb261b846b976f0e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1c7f989d20f4b4dc6d0",
+    "url": "/js/app.7a7dce7e.js"
+  },
+  {
+    "revision": "6e5cf45d66448e551f03",
+    "url": "/js/chunk-vendors.6be0fa3a.js"
+  },
+  {
+    "revision": "aa035da393ea0d48600cfb81d32f9aa7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
